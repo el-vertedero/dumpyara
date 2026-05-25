ff44163358f00ed524df115ed9ba476899a29bc5
.. <..@..com>
2026-05-25 13:08:38 +0200

    Track our own Firmware_extractor

--- dumpyara.sh
+++ dumpyara.sh
@@ -172,7 +172,7 @@ if [[ -d "${PWD}/external/Firmware_extractor" ]]; then
     git -C "${PWD}"/external/Firmware_extractor pull --recurse-submodules --rebase
 else
     LOGI "Cloning 'Fimrware_extractor' to 'external/'..."
-    git clone -q --recurse-submodules https://github.com/AndroidDumps/Firmware_extractor "${PWD}"/external/Firmware_extractor
+    git clone -q --recurse-submodules https://github.com/R0rt1z2/Firmware_extractor "${PWD}"/external/Firmware_extractor
 fi
 
 # Extract input via 'Firmware_extractor'
